a16271b56e702ff4fca7abdecd7f603217233d04
JoanVC <36604048+JoanVC100@users.noreply.github.com>
2024-03-06 07:57:42 +0000

    MCP23008 peripheral

---
+++ src/peripheral-mcp23008.c
@@ -0,0 +1,316 @@
+#include "../include/peripheral-mcp23008.h"
+
+#include <stdio.h>
+#include <errno.h>
+
+// Registers
+#define IODIR_REGISTER		0x00
+#define IPOL_REGISTER		0x01
+#define GPINTEN_REGISTER	0x02
+#define DEFVAL_REGISTER		0x03
+#define INTCON_REGISTER		0x04
+#define IOCON_REGISTER		0x05
+#define GPPU_REGISTER		0x06
+#define INTF_REGISTER		0x07
+#define INTCAP_REGISTER		0x08
+#define GPIO_REGISTER		0x09
+#define OLAT_REGISTER		0x0a
+
+// Registers values and masks
+#define IOCON_INTPOL		0x02
+#define IOCON_ODR		0x04
+#define IOCON_DISSLW		0x10
+#define IOCON_SEQOP		0x20
+
+static inline int write_reg(i2c_interface_t* i2c, uint8_t addr, uint8_t reg, uint8_t value) {
+	FAST_CREATE_I2C_WRITE(read_order_reg, reg, value);
+	return i2c_write(i2c, addr, &read_order_reg);
+}
+
+static int mcp23008_reset(i2c_interface_t* i2c, uint8_t addr) {
+	int i2c_ret;
+
+	i2c_ret = write_reg(i2c, addr, IODIR_REGISTER, 0xFF);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, IPOL_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, GPINTEN_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, DEFVAL_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, INTCON_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, IOCON_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, GPPU_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, INTF_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, INTCAP_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, GPIO_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	i2c_ret = write_reg(i2c, addr, OLAT_REGISTER, 0x00);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	return 0;
+}
+
+int mcp23008_init(i2c_interface_t* i2c, uint8_t addr) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+	FAST_CREATE_I2C_WRITE(read_order_iocon_reg, IOCON_REGISTER);
+	FAST_CREATE_I2C_WRITE(read_order_gppu_reg, GPPU_REGISTER);
+
+	uint8_t iocon, gppu;
+	i2c_read_t read_iocon_reg = {.buff=&iocon, .len=1};
+	i2c_read_t read_gppu_reg = {.buff=&gppu, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_iocon_reg, &read_iocon_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+	i2c_ret = i2c_write_then_read(i2c, addr, &read_order_gppu_reg, &read_gppu_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	if ((iocon == (IOCON_SEQOP | IOCON_ODR)) && (gppu == 0x00)) {
+		// Already initialized
+		errno = EALREADY;
+		return 1;
+	}
+
+	i2c_ret = mcp23008_reset(i2c, addr);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	// Sequential operation disabled, and INT as open-drain
+	i2c_ret = write_reg(i2c, addr, IOCON_REGISTER, IOCON_SEQOP | IOCON_ODR);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	errno = 0;
+	return 0;
+}
+
+int mcp23008_deinit(i2c_interface_t* i2c, uint8_t addr) {
+        if (i2c == NULL) {
+	        errno = EFAULT;
+	        return -1;
+        }
+        if (addr >= 128) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+        FAST_CREATE_I2C_WRITE(read_order_iocon_reg, IOCON_REGISTER);
+	FAST_CREATE_I2C_WRITE(read_order_gppu_reg, GPPU_REGISTER);
+
+	uint8_t iocon, gppu;
+	i2c_read_t read_iocon_reg = {.buff=&iocon, .len=1};
+	i2c_read_t read_gppu_reg = {.buff=&gppu, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_iocon_reg, &read_iocon_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+	i2c_ret = i2c_write_then_read(i2c, addr, &read_order_gppu_reg, &read_gppu_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+        if (iocon == 0x00 && gppu == 0x00) {
+		// Not initialized
+		errno = EALREADY;
+		return 1;
+	}
+
+        i2c_ret = mcp23008_reset(i2c, addr);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+        errno = 0;
+        return 0;
+}
+
+
+int mcp23008_set_pin_mode(i2c_interface_t* i2c, uint8_t addr, uint8_t index, uint8_t mode) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128 || index >= 8 || mode >= 2) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+	FAST_CREATE_I2C_WRITE(read_order_iodir_reg, IODIR_REGISTER);
+
+	uint8_t iodir;
+	i2c_read_t read_iodir_reg = {.buff=&iodir, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_iodir_reg, &read_iodir_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	if (mode & 0x01) {
+		iodir |= (1 << index);
+	}
+	else {
+		iodir &= ~(1 << index);
+	}
+
+	i2c_ret = write_reg(i2c, addr, IODIR_REGISTER, iodir);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	errno = 0;
+	return 0;
+}
+
+int mcp23008_read(i2c_interface_t* i2c, uint8_t addr, uint8_t index, uint8_t* value) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128 || index >= 8) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+	FAST_CREATE_I2C_WRITE(read_order_gpio_reg, GPIO_REGISTER);
+
+	i2c_read_t read_gpio_reg = {.buff=value, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_gpio_reg, &read_gpio_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	*value = (*value >> index) & 0b00000001;
+
+	errno = 0;
+	return 0;
+}
+
+int mcp23008_write(i2c_interface_t* i2c, uint8_t addr, uint8_t index, uint8_t value) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128 || index >= 8) {
+	        errno = EINVAL;
+	        return -1;
+        }
+	value = value > 0 ? 1 : 0;
+
+
+	FAST_CREATE_I2C_WRITE(read_order_gpio_reg, GPIO_REGISTER);
+
+	uint8_t gpio;
+	i2c_read_t read_gpio_reg = {.buff=&gpio, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_gpio_reg, &read_gpio_reg);
+
+	if (!value) {
+		gpio &= ~(1 << index);
+	}
+	else {
+		gpio |= (1 << index);
+	}
+
+	i2c_ret = write_reg(i2c, addr, GPIO_REGISTER, gpio);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	errno = 0;
+	return 0;
+}
+
+int mcp23008_read_all(i2c_interface_t* i2c, uint8_t addr, uint8_t* value) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+	FAST_CREATE_I2C_WRITE(read_order_gpio_reg, GPIO_REGISTER);
+
+	i2c_read_t read_gpio_reg = {.buff=value, .len=1};
+
+	int i2c_ret = i2c_write_then_read(i2c, addr, &read_order_gpio_reg, &read_gpio_reg);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	errno = 0;
+	return 0;
+}
+
+int mcp23008_write_all(i2c_interface_t* i2c, uint8_t addr, uint8_t value) {
+	if (i2c == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+	if (addr >= 128) {
+	        errno = EINVAL;
+	        return -1;
+        }
+
+	int i2c_ret = write_reg(i2c, addr, GPIO_REGISTER, value);
+	if (i2c_ret != 0) {
+		return i2c_ret;
+	}
+
+	errno = 0;
+	return 0;
+}
---
+++ tests/peripheral-mcp23008.c
@@ -0,0 +1,187 @@
+#include <i2c-interface.h>
+#include <peripheral-mcp23008.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MCP23008_ADDRESS 0x20
+#define IODIR_REGISTER 0x00
+#define GPIO_REGISTER 0x09
+
+#include <unity.h>
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void mcp23008_init_sanity_check() {
+	i2c_interface_t* i2c = i2c_init(1);
+	TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_init(NULL, 0xFF), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EFAULT, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_init(NULL, 0x00), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EFAULT, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_init(i2c, 0xFF), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EINVAL, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_init(i2c, 0x00), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EIO, errno, strerror(errno));
+
+
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_deinit(&i2c), strerror(errno));
+	TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+void mcp23008_deinit_sanity_check() {
+	i2c_interface_t* i2c = i2c_init(1);
+	TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_deinit(NULL, 0xFF), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EFAULT, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_deinit(NULL, 0x00), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EFAULT, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_deinit(i2c, 0xFF), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EINVAL, errno, strerror(errno));
+
+	TEST_ASSERT_EQUAL_MESSAGE(-1, mcp23008_deinit(i2c, 0x00), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(EIO, errno, strerror(errno));
+
+
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_deinit(&i2c), strerror(errno));
+	TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+void mcp23008_init_deinit_cycle() {
+	i2c_interface_t* i2c = i2c_init(1);
+        TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+
+        mcp23008_deinit(i2c, MCP23008_ADDRESS);
+
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_init(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_deinit(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+
+        TEST_ASSERT_MESSAGE(i2c_deinit(&i2c) == 0, strerror(errno));
+        TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+void mcp23008_set_pin_mode_test() {
+	i2c_interface_t* i2c = i2c_init(1);
+        TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_init(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+        // Set pin to output
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_set_pin_mode(i2c, MCP23008_ADDRESS, 0x07, 0x00), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+        FAST_CREATE_I2C_WRITE(read_order_iodir_reg, IODIR_REGISTER);
+	uint8_t iodir;
+	i2c_read_t read_iodir_reg = {.buff=&iodir, .len=1};
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_iodir_reg, &read_iodir_reg), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(0b01111111, iodir, strerror(errno));
+
+
+	// Set pin to input
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_set_pin_mode(i2c, MCP23008_ADDRESS, 0x07, 0x01), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_iodir_reg, &read_iodir_reg), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(0b11111111, iodir, strerror(errno));
+
+
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_deinit(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+        TEST_ASSERT_MESSAGE(i2c_deinit(&i2c) == 0, strerror(errno));
+        TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+void mcp23008_read_test() {
+	i2c_interface_t* i2c = i2c_init(1);
+        TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_init(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_set_pin_mode(i2c, MCP23008_ADDRESS, 0x05, 0x01), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+        FAST_CREATE_I2C_WRITE(read_order_gpio_reg, GPIO_REGISTER);
+	uint8_t gpio_reg;
+	i2c_read_t read_gpio_reg = {.buff=&gpio_reg, .len=1};
+
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_gpio_reg, &read_gpio_reg), strerror(errno));
+
+	uint8_t value = 0b11111111;
+	TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_read(i2c, MCP23008_ADDRESS, 5, &value), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(value, (gpio_reg >> 5) & 0b00000001, strerror(errno));
+	// printf("Value in channel 5: %d; Register value: %d\n", value, gpio_reg);
+
+
+
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_deinit(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+        TEST_ASSERT_MESSAGE(i2c_deinit(&i2c) == 0, strerror(errno));
+        TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+void mcp23008_write_test() {
+	i2c_interface_t* i2c = i2c_init(1);
+        TEST_ASSERT_NOT_NULL_MESSAGE(i2c, strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_init(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_set_pin_mode(i2c, MCP23008_ADDRESS, 0x05, 0x00), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+
+        FAST_CREATE_I2C_WRITE(read_order_gpio_reg, GPIO_REGISTER);
+	uint8_t gpio_reg;
+	i2c_read_t read_gpio_reg = {.buff=&gpio_reg, .len=1};
+
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_gpio_reg, &read_gpio_reg), strerror(errno));
+	TEST_ASSERT_EQUAL(0, gpio_reg);
+
+	// Write 1
+	TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_write(i2c, MCP23008_ADDRESS, 0x05, 1), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_gpio_reg, &read_gpio_reg), strerror(errno));
+	TEST_ASSERT_EQUAL(0b00100000, gpio_reg);
+
+	// Write 0
+	TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_write(i2c, MCP23008_ADDRESS, 0x05, 0), strerror(errno));
+	TEST_ASSERT_EQUAL_MESSAGE(0, i2c_write_then_read(i2c, MCP23008_ADDRESS, &read_order_gpio_reg, &read_gpio_reg), strerror(errno));
+	TEST_ASSERT_EQUAL(0, gpio_reg);
+
+
+        TEST_ASSERT_EQUAL_MESSAGE(0, mcp23008_deinit(i2c, MCP23008_ADDRESS), strerror(errno));
+        TEST_ASSERT_EQUAL_MESSAGE(0, errno, strerror(errno));
+        TEST_ASSERT_MESSAGE(i2c_deinit(&i2c) == 0, strerror(errno));
+        TEST_ASSERT_NULL_MESSAGE(i2c, strerror(errno));
+}
+
+
+int main() {
+	UNITY_BEGIN();
+
+	RUN_TEST(mcp23008_init_sanity_check);
+	RUN_TEST(mcp23008_deinit_sanity_check);
+	RUN_TEST(mcp23008_init_deinit_cycle);
+
+	RUN_TEST(mcp23008_set_pin_mode_test);
+
+	RUN_TEST(mcp23008_read_test);
+
+	RUN_TEST(mcp23008_write_test);
+
+        return UNITY_END();
+}
