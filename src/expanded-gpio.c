8ad46ad9f64e7b4d04938ffde46644f8bd4c8c95
JoanVC <36604048+JoanVC100@users.noreply.github.com>
2024-03-21 09:26:02 +0100

    Make a bigger delay before initializing MCP23017 (sometimes the 14 IOs fails)

--- src/expanded-gpio.c
+++ src/expanded-gpio.c
@@ -119,7 +119,7 @@ int initExpandedGPIO(bool restart_peripherals) {
 	 * delay, attempting to communicate with the devices immediately after a reset
 	 * may result in a NACK, causing the program to immediately fail.
 	 */
-	usleep(60 * 1000);
+	usleep(80 * 1000);
 
 	ret = init_device(mcp23017_init, mcp23017_deinit, MCP23017, NUM_MCP23017, restart_peripherals);
 	assert(ret != FIRST_INIT);
